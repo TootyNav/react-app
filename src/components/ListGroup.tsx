6996b0af1e783718d43bd842f05bcb1490736188
Navid Shiraj <navid2007@hotmail.com>
2023-10-09 20:00:38 +0700

    show list of cities in the view

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  const items = ["New York", "London", "Manchester", "Bangkok"];
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
