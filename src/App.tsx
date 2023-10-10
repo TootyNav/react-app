4b6df8cc5989aad1533f6cd53108802d4d0aef40
Navid Shiraj <navid2007@hotmail.com>
2023-10-10 18:31:42 +0700

    add delegate funcction to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const items = ["New York", "London", "Manchester", "Bangkok"];
 
+  const handleSelectItem = (item: string) => console.log(item);
+
   return (
     <div>
-      <ListGroup items={items} heading="List of Cities"></ListGroup>
+      <ListGroup
+        items={items}
+        heading="List of Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
 }
