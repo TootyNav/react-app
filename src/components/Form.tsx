59be60efe466fcc58e9282efa17ac16b9b284422
Navid Shiraj <navid2007@hotmail.com>
2023-12-31 19:48:13 +0000

    Add package hookform/resolvers to resolve zod validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,24 @@
 import { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  name: z.string().min(3),
-  age: z.number().min(3),
+  name: z.string().min(3, { message: "Name must be at least 3 charactors." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
 });
 
 type formData = z.infer<typeof schema>;
 
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formData>({ resolver: zodResolver(schema) });
+
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -25,6 +33,7 @@ const Form = () => {
           type="text"
           className="form-control"
         />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
 
       <div className="mb-3">
@@ -32,11 +41,12 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
 
       <button className="btn btn-primary" type="submit">
