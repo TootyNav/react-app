a47f63b7f5cc8e19c9d2866596f505b827682bf9
Navid Shiraj <navid2007@hotmail.com>
2023-12-31 15:14:56 +0000

    Use useState for form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,53 @@
 import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
+
+  const handleSybmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log(person);
+  };
+
   return (
     <form onSubmit={handleSybmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({
+              ...person,
+              name: event.target.value,
+            })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({
+              ...person,
+              age: event.target.value,
+            })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
 
       <button className="btn btn-primary" type="submit">
