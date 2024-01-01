7d8a20e990822b5e0c6ab9d10e501d48c8a59f69
Navid Shiraj <navid2007@hotmail.com>
2024-01-01 13:09:14 +0000

    create a new array and display it in a table

--- src/components/ExpenseTracker.tsx
+++ src/components/ExpenseTracker.tsx
@@ -1,6 +1,7 @@
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 const schema = z.object({
   description: z
@@ -21,61 +22,97 @@ const ExpenseTracker = () => {
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
+  const expenseArray: formData[] = [
+    {
+      description: "Milk",
+      amount: 2,
+      category: "Groceries",
+    },
+  ];
+  const [expenseArrayState, setExpenseArrayState] =
+    useState<formData[]>(expenseArray);
+
+  console.log(expenseArrayState);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Description
-        </label>
-        <input
-          {...register("description")}
-          id="description"
-          type="text"
-          className="form-control"
-        />
-        {errors.description && (
-          <p className="text-danger">{errors.description.message}</p>
-        )}
-      </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            {...register("description")}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description.message}</p>
+          )}
+        </div>
 
-      <div className="mb-3">
-        <label htmlFor="amount" className="form-label">
-          Amount
-        </label>
-        <input
-          {...register("amount", { valueAsNumber: true })}
-          id="amount"
-          type="number"
-          className="form-control"
-        />
-        {errors.amount && (
-          <p className="text-danger">{errors.amount.message}</p>
-        )}
-      </div>
+        <div className="mb-3">
+          <label htmlFor="amount" className="form-label">
+            Amount
+          </label>
+          <input
+            {...register("amount", { valueAsNumber: true })}
+            id="amount"
+            type="number"
+            className="form-control"
+          />
+          {errors.amount && (
+            <p className="text-danger">{errors.amount.message}</p>
+          )}
+        </div>
 
-      <div className="mb-3">
-        <label htmlFor="category" className="form-label">
-          Category
-        </label>
-        <select
-          {...register("category")}
-          id="category"
-          className="form-control"
-        >
-          <option value=""></option>
-          <option>Groceries</option>
-          <option>Utilities</option>
-          <option>Entertainment</option>
-        </select>
-        {errors.category && (
-          <p className="text-danger">{errors.category.message}</p>
-        )}
-      </div>
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <select
+            {...register("category")}
+            id="category"
+            className="form-control"
+          >
+            <option value=""></option>
+            <option>Groceries</option>
+            <option>Utilities</option>
+            <option>Entertainment</option>
+          </select>
+          {errors.category && (
+            <p className="text-danger">{errors.category.message}</p>
+          )}
+        </div>
 
-      <button className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </form>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Description</th>
+              <th scope="col">Amount</th>
+              <th scope="col">Category</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenseArrayState.map((expense, index) => (
+              <tr key={index}>
+                <td>{expense.description}</td>
+                <td>{expense.amount}</td>
+                <td>{expense.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
